be9967cd95ea3c9e43fb33702e102d3a82106f66
AnandSutariya08 <anandsutariya83@gmail.com>
2026-07-09 17:35:19 +0530

    video chnages

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -304,7 +304,7 @@ const ProductDetail = () => {
                             muted
                             loop
                             playsInline
-                            preload="auto"
+                            preload={isActive ? "auto" : "none"}
                             watermark
                           />
                         ) : (
